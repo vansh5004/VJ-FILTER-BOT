96f32a9ea5d28e468879d8f243fc642949d98d1d
Vansh Kumar <145826508+vansh5004@users.noreply.github.com>
2023-11-27 13:19:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -62,13 +62,13 @@ async def start(client, message):
         m=await message.edit_text(
             text="sʏsᴛᴇᴍ ᴘᴀᴀᴅ ᴅᴇɴɢᴇ ⚡️"
         ) 
-        m=await query.message.edit_text(
+        await query.message.edit_text(
             text="Cʜᴇᴄᴋɪɴɢ = ▮▯▯"
         )
-        m=await query.message.edit_text(
+        await query.message.edit_text(
             text="Cʜᴇᴄᴋɪɴɢ = ▮▮▯"
         )
-        m=await query.message.edit_text(
+        await query.message.edit_text(
             text="Cʜᴇᴄᴋɪɴɢ = ▮▮▮"
         )
         await asyncio.sleep(1)
